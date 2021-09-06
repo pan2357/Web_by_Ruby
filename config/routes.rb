bc72cdce0645c4e8477810e19a1ae11a2d2fdf32
pan2357 <panya.dora@gmail.com>
2021-09-06 19:51:52 +0700

    Revert "Homework 2"
    This reverts commit 2f4d4c6590db776c67ec1d8b47e8a3dd4d9d70da.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'user_and_post/create_post'
-  get 'user_and_post/read_post'
   get 'ex2/loop'
   get 'main/welcome'
   get 'main/contact'
   get 'welcome', to: 'main#welcome'
   get 'contact_us', to: 'main#contact'
   get 'loop', to: 'ex2#loop'
-  get 'post/create', to: 'user_and_post#create_post'
-  get 'user/read', to: 'user_and_post#read_post'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
